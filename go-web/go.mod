0ffde70186d342a98eaf60ccd944aada1c5371a7
leoyim <leo.wbyan@gmail.com>
2023-08-15 22:03:49 +0800

    second try

---
+++ go-web/go.mod
@@ -0,0 +1,8 @@
+module go-web
+go 1.18
+require (
+	goo v0.1
+)
+replace (
+	goo => ./goo
+)
--- go-web/main.go
+++ go-web/main.go
@@ -6,18 +6,36 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
+type Engine struct {
 }
 
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
 }
+
+func main() {
+	//http.HandleFunc("/", indexHandler)
+	//http.HandleFunc("/hello", helloHandler)
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
+}
+
+//func indexHandler(w http.ResponseWriter, req *http.Request) {
+//	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+//}
+//
+//func helloHandler(w http.ResponseWriter, req *http.Request) {
+//	for k, v := range req.Header {
+//		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+//	}
+//}
